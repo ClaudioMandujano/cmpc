b44d40aa29ae1ca2d408e11d09f345d6239c50f2
ClaudioMandujano <claudio.mandujano@gmail.com>
2023-05-04 19:47:45 -0300

    add lightbox

--- js/galeria.js
+++ js/galeria.js
@@ -30,48 +30,7 @@ function changeImage(imageSrc, imageNumber) {
   // Cambiar miniatura activa
   var activeThumbnail = document.querySelector(".thumbnail-img.active");
   activeThumbnail.classList.remove("active");
-  var newThumbnail = document.querySelector(".thumbnail-img:nth-of-type(" + imageNumber + ")");
-  newThumbnail.classList.add("active");
+
+  var newActiveThumbnail = document.querySelector(".thumbnail-img[alt='Miniatura " + imageNumber + "']");
+  newActiveThumbnail.classList.add("active");
 }
-
-// Función de rotación automática
-var intervalId = setInterval(function() {
-  nextImage();
-}, 3000);
-
-// Detener la rotación al hacer hover en las miniaturas
-var thumbnailContainer = document.querySelector(".thumbnail-container-2");
-thumbnailContainer.addEventListener("mouseover", function() {
-  clearInterval(intervalId);
-});
-
-// Reanudar la rotación al quitar el hover de las miniaturas
-thumbnailContainer.addEventListener("mouseout", function() {
-  intervalId = setInterval(function() {
-    nextImage();
-  }, 3000);
-});
-
-// Función para mover las miniaturas
-function moveThumbnails() {
-  const thumbnailBox = document.querySelector('.thumbnail-box');
-  const thumbnails = thumbnailBox.querySelectorAll('.thumbnail');
-  const thumbnailWidth = thumbnails[0].offsetWidth;
-  const totalWidth = thumbnailWidth * thumbnails.length;
-
-  // Ajusta el ancho del contenedor de miniaturas
-  thumbnailBox.style.width = totalWidth + 'px';
-
-  // Crea una animación que desplaza las miniaturas hacia la izquierda
-  thumbnailBox.animate([
-    { transform: `translateX(0)` },
-    { transform: `translateX(-${totalWidth}px)` }
-  ], {
-    duration: 5000,
-    iterations: Infinity,
-    easing: 'linear'
-  });
-}
-
-// Llama a la función para mover las miniaturas
-moveThumbnails();
